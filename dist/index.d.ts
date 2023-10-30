4939b4e86e283c9d972008159f043455f7e3bc6e
tiago-dias-laureano <tiagodiaslaureano32@gmail.com>
2023-10-30 19:30:19 -0300

    build

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import { PixJSProps, PixJSPropsImage } from "./types";
+export declare function CopyAndPaste(props: PixJSProps): string;
+export declare function QrCodeTerminal(props: PixJSProps): string;
+export declare function QrCodeImage(props: PixJSPropsImage): void;
---
+++ dist/types/index.d.ts
@@ -0,0 +1,17 @@
+export type PixJSProps = {
+    name: string;
+    key: string;
+    amount: number;
+    city: string;
+    id: string;
+    type: "cpf" | "cnpj" | "phone" | "random" | "email";
+};
+export type PixJSPropsImage = {
+    name: string;
+    key: string;
+    amount: number;
+    city: string;
+    id: string;
+    type: "cpf" | "cnpj" | "phone" | "random" | "email";
+    path?: string;
+};
